8a6a078ce64b6c90ce35c6b0f460672e37cc118e
Peace <devbypeace@gmail.com>
2026-02-03 21:23:25 +0100

    feat: setup routing with protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+/////////////Pages
+import Login from "./pages/Login";
+import Homepage from "./pages/Homepage";
+import Details from "./pages/Details";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthProvider } from "./contexts/AuthContext";
+
 function App() {
   return (
-    <div className="text-3xl text-center">
-      Hello World
-    </div>
-  )
+    <AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Homepage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/details"
+          element={
+            <ProtectedRoute>
+              <Details />
+            </ProtectedRoute>
+          }
+        />
+
+
+
+
+
+        <Route >
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
